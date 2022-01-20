5ce311f2566eb43cf30947c077a8c1884a0ae190
saiharshavarma <saiharshavarma@outlook.com>
2022-01-20 13:29:57 +0530

    csv file location

--- website/views.py
+++ website/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 import pandas as pd
 import csv
 
+registrations_file = "CCS Registrations.csv"
+
 def home(request):
     result = None
     context = {"result" : result}
@@ -15,10 +17,10 @@ def register(request):
         regno = request.POST.get('regno')
         email = request.POST.get('email')
         phone = request.POST.get('phone')
-        dataset = pd.read_csv("Result.csv", names=['Name', 'Registration'])
+        dataset = pd.read_csv(registrations_file, names=['Name', 'Registration'])
         dataset = pd.DataFrame(dataset)
         if regno not in list(dataset['Registration']):
-            with open('Result.csv', 'a', newline='') as csvfile:
+            with open(registrations_file, 'a', newline='') as csvfile:
                 wrt = csv.writer(csvfile)
                 wrt.writerow([name, regno, email, phone])
         else:
@@ -32,7 +34,7 @@ def ccsresults(request):
     result = None
     if request.method == 'POST':
         reg = request.POST.get('registration')
-        dataset = pd.read_csv("Result.csv", names=['Name', 'Registration'])
+        dataset = pd.read_csv(registrations_file, names=['Name', 'Registration'])
         dataset = pd.DataFrame(dataset)
         if reg in list(dataset['Registration']):
             result = "Pass"
